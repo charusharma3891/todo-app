7d7e9809360d09971ec9a8a5fd97c89db5738af1
charusharma3891 <charusharma3891@gmail.com>
2022-02-03 06:55:56 +0530

    Bootstrap included.
    Bootstrap included.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoapp';
+  closeResult = '';
+  constructor(private addNoteModal: NgbModal) { }
+  modalOpen = false;
+  openAddNote(content: any) {
+    if (this.modalOpen) return;
+    this.modalOpen = true;
+    this.addNoteModal.open(content).result.then((result: any) => {
+        this.modalOpen = false;
+        this.closeResult = `Closed with: ${result}`;
+    }, (reason: any) => {
+        this.modalOpen = false;
+        this.closeResult =
+          `Dismissed ${this.getDismissReason(reason)}`;
+    });
+
+  }
+
+  private getDismissReason(reason: any) {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
+
